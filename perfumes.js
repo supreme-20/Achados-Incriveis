7f02998aed068e8fa608f2b5189b04ac1974ceb4
supreme-20 <zeusagar.2018@gmail.com>
2026-09-03 09:32:47 -0300

    perfumes2

--- perfumes.js
+++ perfumes.js
@@ -5,6 +5,16 @@ const perfumes = [
         quantidade: "75 ml",
         imagem: "lily-blanche.jpg",
         preco: 229.90,
+        link: "#",
+    },
+
+    {
+        nome: "Nativa SPA Lilac",
+        tipo: "Body Splash Desodorante Colônia",
+        quantidade: "200 ml",
+        imagem: "nativa-spa-lilac.jpg",
+        preco: 109.90,
         link: "#"
     }
+
 ];
