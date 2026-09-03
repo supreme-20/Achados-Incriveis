917e9d08efc52786902992b8aae2f8c45557f670
supreme-20 <zeusagar.2018@gmail.com>
2026-09-03 14:12:46 -0300

    perfumes

--- perfumes.js
+++ perfumes.js
@@ -15,6 +15,33 @@ const perfumes = [
         imagem: "nativa-spa-lilac.jpg",
         preco: 109.90,
         link: "#"
-    }
+    },
+
+    {
+    nome: "Eudora H",
+    tipo: "Desodorante Colônia",
+    quantidade: "100 ml",
+    imagem: "https://res.cloudinary.com/beleza-na-web/image/upload/w_500,h_500,f_auto,fl_progressive,q_auto:good/v1/imagens/product/E94051/67ab7da5-4d03-43f3-98fc-11673e32cd32-eudora-h-desodorante-colonia-100ml-v2-e94051.jpg",
+    preco: 124.90,
+    link: "#"
+    },
+
+    {
+    nome: "Instance Frutas Vermelhas",
+    tipo: "Creme Hidratante Desodorante Corporal",
+    quantidade: "400 ml",
+    imagem: "https://res.cloudinary.com/beleza-na-web/image/upload/w_500,h_500,f_auto,fl_progressive,q_auto:good/v1/imagens/product/E87126/80407882-eb95-4c43-9f12-32fc51ddc069-e87126-instance-frutas-vermelhas-creme-400ml-1.jpg",
+    preco: 44.90,
+    link: "#"
+    },
+
+    {
+    nome: "Celebre Sua Força",
+    tipo: "Desodorante Colônia Masculino",
+    quantidade: "100 ml",
+    imagem: "https://res.cloudinary.com/beleza-na-web/image/upload/w_500,h_500,f_auto,fl_progressive,q_auto:good/v1/imagens/products/B83059/2000x2000_CARD_PDP_83059_1.jpg",
+    preco: 124.90,
+    link: "#"
+    },
 
 ];
